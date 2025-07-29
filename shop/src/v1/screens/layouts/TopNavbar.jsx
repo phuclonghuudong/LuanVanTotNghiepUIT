8cc83ab659bb92dacedf7fff7e4cff700a335d0c
hungmanh02 <domanh462@gmail.com>
2025-07-29 22:14:45 +0700

    fix error header merge frontend

--- shop/src/v1/screens/layouts/TopNavbar.jsx
+++ shop/src/v1/screens/layouts/TopNavbar.jsx
@@ -1,6 +1,7 @@
+import React from 'react'
+
 const TopNavbar = () => {
   return (
-<div>
       <div className='bg-black'>
         <div className='container mx-auto xl:px-[50px] flex  items-center xl:justify-between justify-center min-h-[45px]  text-white'>
             <div>
@@ -17,17 +18,9 @@ const TopNavbar = () => {
                 </select>
             </div>
         
-
         </div>
-        <div className=" hidden xl:block">
-          <select name="" id="">
-            <option value="">Việt Nam</option>
-            <option value="">English</option>
-          </select>
-        </div>
-      </div>
     </div>
-  );
-};
+  )
+}
 
-export default TopNavbar;
+export default TopNavbar
